0076c630bb58e69d73cae370a71c7900845aac95
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-04-26 19:07:43 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,103 @@
+//Array of objects
+const quiz = [
+    {
+        q:"I ___ from France.",
+        options:["am", "is", "are"],
+        answer: 0
+    },
+    {
+     q:"This is my friend. ___ name is John.",
+        options:["Him", "His", "He"],
+        answer: 1   
+    },
+    {
+        q:"Mike is ___ .",
+        options:["friend my sister", "my sister friend", "my sister's friend"],
+        answer: 2
+    },
+    {
+        q:"My brother is ___ artist.",
+        options:["an", "a", "the"],
+        answer: 0
+    },
+    {
+        q:"___ 20 desks in the classroom.",
+        options:["There is", "There are", "This is"],
+        answer: 1
+    },
+    {
+        q:"Paul ___ romantic films.",
+        options:["doesn't likes","don't like", "doesn't like"],
+        answer: 2
+    },
+    {
+        q:"Sorry, I can't talk. I ___ right now.",
+        options:["'m driving", "drive", "driving"],
+        answer: 0
+    },
+    {
+        q:"She ___ at school last week.",
+        options:["weren't", "wasn't", "isn't"],
+        answer: 1
+    },
+    {
+        q:"I ___ the film last night.",
+        options:["likes", "like", "liked"],
+        answer: 2
+    },
+    {
+        q:"___ a piece of cake? - No, thank you.",
+        options:["Would you like", "Are you like", "Do you like"],
+        answer: 0
+    },
+    {
+        q:"The living room is ___ than the bedroom.",
+        options:["more bigger", "bigger", "more big"],
+        answer: 1
+    },
+    {
+        q:"The car is very old. We are going ___ a new car soon.",
+        options:["buying", "buy", "to buy"],
+        answer: 2
+    },
+    {
+        q:"Kate is a vegetarian. She ___ meat.",
+        options:["never eats", "often eats", "sometimes eats"],
+        answer: 0
+    },
+    {
+        q:"There aren't ___ buses late in the evening.",
+        options:["some", "any", "a"],
+        answer: 1
+    },
+    {
+        q:"The car park is ___ to the restaurant.",
+        options:["in front", "behind", "next"],
+        answer: 2
+    },
+    {
+        q:"Lucy ___ shopping every day.",
+        options:["goes", "is going", "go"],
+        answer: 0
+    },
+    {
+        q:"They ___ in the park when it started to rain heavily.",
+        options:["walked", "were walking", "was walking"],
+        answer: 1
+    },
+    {
+        q:"I ___ when my mum came home.",
+        options:["slept", "sleep", "was sleeping"],
+        answer: 2
+    },
+    {
+        q:"It's ___ (11.40)",
+        options:["twenty to twelve", "twenty past twelve", "fourty past eleven "],
+        answer: 0
+    },
+    {
+        q:"I haven't got ___ .",
+        options:["many money", "much money", "some money"],
+        answer: 1
+    }
+]
